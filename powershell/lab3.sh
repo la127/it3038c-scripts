02ad770c79a26a5631da8b8b44b2b1d834fc92c3
Levi <100445409+lh1207@users.noreply.github.com>
2023-09-24 22:15:39 -0400

    Create lab3.sh

---
+++ powershell/lab3.sh
@@ -0,0 +1,23 @@
+# Get machine's IP address
+$IPAddress = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1).IPv4Address.IPAddressToString
+
+# Get username
+$Username = $env:USERNAME
+
+# Get hostname
+$Hostname = $env:COMPUTERNAME
+
+# Get PowerShell version (helps w/ ensuring successful execution)
+$PSVersion = $PSVersionTable.PSVersion
+
+# Get current date
+$CurrentDate = Get-Date -Format "dddd, MMMM dd, yyyy"
+
+# Create $Body variable
+$Body = "This machine's IP is $IPAddress. User is $Username. Hostname is $Hostname. PowerShell Version $PSVersion. Today's Date is $CurrentDate"
+
+# Save $Body to text file
+$Body | Out-File -FilePath "output.txt"
+
+# Data saved
+Write-Host "Data saved as output.txt"
