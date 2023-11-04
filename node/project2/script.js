73e6ceee87e4d5d7c938df536fb52c0ad8a5d255
Levi <100445409+lh1207@users.noreply.github.com>
2023-11-04 03:02:10 -0400

    combine html and js into one file

--- node/project2/script.js
+++ node/project2/script.js
@@ -1,4 +1,6 @@
-const si = require('systeminformation'); // npm install systeminformation
+const http = require('http');
+const si = require('systeminformation');
+const opn = require('opn');
 
 async function fetchHardwareInfo() {
   try {
@@ -10,45 +12,82 @@ async function fetchHardwareInfo() {
     const osData = await si.osInfo();
     const networkData = await si.networkInterfaces();
 
-    // object for data
     const hardwareInfo = {
-      cpu: cpuData,
-      memory: memData,
-      disks: diskData,
-      battery: batteryData,
-      graphics: graphicsData,
-      os: osData,
-      network: networkData,
+      cpu: cpuData.manufacturer,
+      memory: memData.manufacturer,
+      disks: diskData.map(disk => disk.name),
+      battery: batteryData[0] ? batteryData[0].model : 'N/A',
+      graphics: graphicsData.controllers.map(controller => controller.model),
+      os: osData.distro,
+      network: networkData.map(interface => interface.iface)
     };
 
     return hardwareInfo;
   } catch (error) {
     console.error('Error fetching hardware information:', error);
-    return null;
+    return { error: 'Failed to fetch hardware information' };
   }
 }
-
-async function updateHardwareInfo() {
-  setInterval(async () => {
-    const info = await fetchHardwareInfo();
-    console.log('Updated hardware information:', info);
-  }, 5000);
-}
-
-updateHardwareInfo();
-
-// NodeJS server code
-const http = require('http');
 
 http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-
   if (req.url === '/hardware-info') {
-    const hardwareInfo = await fetchHardwareInfo();
-    res.end(JSON.stringify(hardwareInfo));
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    try {
+      const hardwareInfo = await fetchHardwareInfo();
+      const hardwareNames = Object.values(hardwareInfo).flat().filter(Boolean);
+
+      const htmlResponse = `
+        <html>
+        <head>
+          <title>Hardware Info</title>
+          <style>
+            html, body {
+              height: 100%;
+            }
+            body {
+              margin: 0;
+              padding: 20px;
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: center;
+              align-content: flex-start;
+              gap: 5px;
+              background: linear-gradient(to bottom right, purple, cyan);
+              color: white;
+              font-family: Arial, sans-serif;
+              font-size: 2em;
+            }
+            .name {
+              text-shadow: 2px 2px 2px black;
+            }
+            @media (max-width: 600px) {
+              .name {
+                font-size: 1.5em;
+              }
+            }
+          </style>
+        </head>
+        <body>
+          ${hardwareNames.map(name => `<span class="name">${name}</span>`).join('')}
+        </body>
+        </html>
+      `;
+
+      res.end(htmlResponse);
+    } catch (error) {
+      res.end('<p>Error fetching hardware information</p>');
+    }
   } else {
-    res.end('Invalid request');
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Invalid request</h1>');
   }
-}).listen(3000, () => {
+}).listen(3000, async () => {
   console.log('Server running on port 3000');
+
+  try {
+    await opn('http://localhost:3000/hardware-info');
+  } catch (error) {
+    console.error('Error opening the browser:', error);
+  }
 });
