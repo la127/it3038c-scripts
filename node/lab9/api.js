d56dd9876258d56d0e6d0da769e3174e507473eb
lh1207 <levihuff1207@gmail.com>
2023-11-05 21:39:34 -0500

    Optimize for formatting

--- node/lab9/api.js
+++ node/lab9/api.js
@@ -4,34 +4,37 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-const widgetsData = JSON.parse(fs.readFileSync('widgets.json'));
+const widgetsData = JSON.parse(fs.readFileSync('widgets.json').toString());
+
+// Add ID field to the widgets data
+widgetsData.forEach((widget, index) => {
+    widget.id = index + 1; // Assigning IDs starting from 1
+});
 
 app.get('/', (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/json" });
-  res.end(JSON.stringify(widgetsData));
+    res.status(200).json(widgetsData);
 });
 
 app.get('/blue', (req, res) => {
-  const blueWidgets = widgetsData.filter(widget => widget.color === 'blue');
-  res.json(blueWidgets);
+    const blueWidgets = widgetsData.filter(widget => widget.color === 'blue');
+    res.status(200).json(blueWidgets);
 });
 
 app.get('/widgets/:id', (req, res) => {
-  const widgetId = req.params.id;
-  const widget = widgetsData.find(widget => widget.id === parseInt(widgetId));
+    const widgetId = req.params.id;
+    const widget = widgetsData.find(widget => widget.id === parseInt(widgetId));
 
-  if (widget) {
-    res.json(widget);
-  } else {
-    res.status(404).json({ message: 'Widget not found' });
-  }
+    if (widget) {
+        res.status(200).json(widget);
+    } else {
+        res.status(404).json({ message: 'Widget not found' });
+    }
 });
 
 app.use((req, res) => {
-  res.status(404).send('Data not found');
+    res.status(404).send('Data not found');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
--- python/lab9/fetch.py
+++ python/lab9/fetch.py
@@ -1,44 +1,37 @@
 import requests
-import json  # Import the json module
+import json
+
+# Function to make a request to the Node.js server
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f'Failed to fetch data from {url}')
+        return None
 
 # Fetch all widgets from the Node.js server
 def get_all_widgets():
-    response = requests.get('http://localhost:3000/')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print('Failed to fetch widgets')
-        return None
+    return fetch_data('http://localhost:3000/')
 
 # Fetch blue widgets from the Node.js server
 def get_blue_widgets():
-    response = requests.get('http://localhost:3000/blue')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print('Failed to fetch blue widgets')
-        return None
+    return fetch_data('http://localhost:3000/blue')
 
 # Fetch a specific widget by ID from the Node.js server
 def get_widget_by_id(widget_id):
-    response = requests.get(f'http://localhost:3000/widgets/{widget_id}')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f'Failed to fetch widget with ID {widget_id}')
-        return None
+    return fetch_data(f'http://localhost:3000/widgets/{widget_id}')
 
 # Example usage
 all_widgets = get_all_widgets()
 print("All Widgets:")
-print(all_widgets)
+print(json.dumps(all_widgets, indent=2))
 
 blue_widgets = get_blue_widgets()
 print("Blue Widgets:")
-print(blue_widgets)
+print(json.dumps(blue_widgets, indent=2))
 
-widget_id = 2  # Replace with an existing widget ID
+widget_id = 1  # Replace with an existing widget ID
 widget = get_widget_by_id(widget_id)
 print(f"Widget with ID {widget_id}:")
-print(widget)
-
+print(json.dumps(widget, indent=2))
