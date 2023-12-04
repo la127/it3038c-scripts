67e48b71254b9760fb99dcd5e3794b2369f270e8
lh1207 <justlevixd@gmail.com>
2023-12-03 19:31:53 -0500

    format datetime; add dependencies needed for linux

--- final-project/fetch.py
+++ final-project/fetch.py
@@ -1,6 +1,9 @@
 import requests
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
+# pip install python-gobject
+# pip install python-qt5
 
 BASE_URL = 'http://localhost:3000/api'
 
@@ -14,13 +17,20 @@ def get_player_counts():
 
 def plot_player_counts(player_counts):
     if player_counts:
-        timestamps = [entry['timestamp'] for entry in player_counts]
+        timestamps = [datetime.fromisoformat(entry['timestamp'].replace('Z', '+00:00')) for entry in player_counts]
         counts = [entry['count'] for entry in player_counts]
-        plt.plot(timestamps, counts, marker='o')
+
+        # Create evenly spaced intervals for x-axis ticks
+        x_ticks = np.linspace(0, len(timestamps) - 1, num=len(timestamps))
+        
+        plt.plot(x_ticks, counts, marker='o')
         plt.title('TF2 Player Counts Over Time')
         plt.xlabel('Time')
         plt.ylabel('Player Count')
-        plt.xticks(rotation=45)
+
+        # Set x-axis labels to be the formatted timestamps
+        plt.xticks(x_ticks, [timestamp.strftime('%Y-%m-%d %H:%M:%S') for timestamp in timestamps], rotation=45, ha='right')
+
         plt.tight_layout()
         plt.show()
     else:
